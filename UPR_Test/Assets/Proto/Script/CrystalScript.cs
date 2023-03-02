a6505fd9b3373b1ab9ca695933a16134a4a25dc0
YutaTakaya <yutapon05@gmail.com>
2023-03-03 00:42:27 +0900

    レーザースクリプトの改善

---
+++ UPR_Test/Assets/Proto/Script/CrystalScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalScript : MonoBehaviour
+{
+    [SerializeField] int needLazerNum = 1;  // 破壊に必要なレーザーの本数
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- UPR_Test/Assets/Proto/Script/LightStartPoint.cs
+++ UPR_Test/Assets/Proto/Script/LightStartPoint.cs
@@ -7,6 +7,9 @@ public class LightStartPoint : MonoBehaviour
     [SerializeField] private float lightLineDistance = 1000.0f;  // 光の反射探索距離
     private LineRenderer lineRenderer;
     [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private int refrectTimes = 3;  // 反射回数
+
+    private List<Vector3> refrectPositions;
 
     private bool nowPrismHitFlag;
     private bool oldPrismHitFlag;
@@ -17,43 +20,36 @@ public class LightStartPoint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        refrectPositions = new List<Vector3>();
         lineRenderer = gameObject.GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        lineRenderer.positionCount = 2;
+        // リストの初期化
+        refrectPositions.Clear();
+
         // 始点の取得
-        Vector3 pos1 = this.gameObject.transform.position;
-
-        // 終点を取得するためにRayを用いて当たる場所を取得
-        RaycastHit rayHitObject;
-        Physics.Raycast(pos1, this.gameObject.transform.forward, out rayHitObject, lightLineDistance, wallLayer);
-        Vector3 pos2 = rayHitObject.point;
-
-        // 線の始点、終点の指定
-        lineRenderer.SetPosition(0, pos1);
-        lineRenderer.SetPosition(1, pos2);
+        refrectPositions.Add(transform.position);
+        Vector3 refrectVec = this.gameObject.transform.forward;
 
         bool lightLineRefrectEndFlag = true;
         while (lightLineRefrectEndFlag)
         {
+            // 終点を取得するためにRayを用いて当たる場所を取得
+            RaycastHit rayHitObject;
+            Physics.Raycast(refrectPositions[refrectPositions.Count - 1], refrectVec, out rayHitObject, lightLineDistance, wallLayer);
+            refrectPositions.Add(rayHitObject.point);
+
             // 鏡に当たった場合反射させる
             if (rayHitObject.collider.tag == "Mirror")
             {
-                lineRenderer.positionCount++;
                 // 1個目の光のベクトルを取得
-                Vector3 beforeLightLineVec = pos2 - pos1;
+                Vector3 beforeLightLineVec = refrectPositions[refrectPositions.Count - 1] - refrectPositions[refrectPositions.Count - 2];
 
                 // 反射光の計算
-                Vector3 reflectVec = Vector3.Reflect(beforeLightLineVec, rayHitObject.normal);
-                Physics.Raycast(rayHitObject.transform.position, reflectVec, out rayHitObject, lightLineDistance, wallLayer);
-
-                // 線の始点、終点の指定
-                pos1 = pos2;
-                pos2 = rayHitObject.point;
-                lineRenderer.SetPosition(lineRenderer.positionCount - 1, pos2);
+                refrectVec = Vector3.Reflect(beforeLightLineVec, rayHitObject.normal);
             }
             else
             {
@@ -62,12 +58,13 @@ public class LightStartPoint : MonoBehaviour
                     // プリズムに当たった場合拡散させる
                     case "Prism":
                         hitPrismScript = rayHitObject.transform.GetComponent<PrismScript>();
-                        hitPrismScript.TurnOnPrismLight(pos2 - pos1);
+                        hitPrismScript.TurnOnPrismLight(refrectPositions[refrectPositions.Count - 1] - refrectPositions[refrectPositions.Count - 2]);
                         nowPrismHitFlag = true;
                         break;
 
                     // ゴールに当たった場合クリア扱いにする
                     case "Goal":
+                    case "Player":
                         if (endFlag == false)
                         {
                             GameObject.FindGameObjectWithTag("Manager").GetComponent<GameManager>().ActiveReloadButton();
@@ -82,17 +79,31 @@ public class LightStartPoint : MonoBehaviour
                 // 反射しないのでループ終了
                 lightLineRefrectEndFlag = false;
             }
-
-            if(nowPrismHitFlag == false && oldPrismHitFlag == true)
-            { 
-                oldPrismHitFlag = false;
-                hitPrismScript.TurnOffPrismLight();
-            }
-            else if(nowPrismHitFlag == true)
-            {
-                nowPrismHitFlag = false;
-                oldPrismHitFlag = true;
-            }
         }
+
+        // レーザーの頂点座標の指定
+        lineRenderer.positionCount = refrectPositions.Count;
+        lineRenderer.SetPositions(refrectPositions.ToArray());
+
+        if (nowPrismHitFlag == false && oldPrismHitFlag == true)
+        {
+            oldPrismHitFlag = false;
+            hitPrismScript.TurnOffPrismLight();
+        }
+        else if (nowPrismHitFlag == true)
+        {
+            nowPrismHitFlag = false;
+            oldPrismHitFlag = true;
+        }
+    }
+
+    public bool shotLazer()
+    {
+        for(int i = 0;i + 1 < refrectPositions.Count;i++)
+        {
+
+        }
+
+        return false;
     }
 }
